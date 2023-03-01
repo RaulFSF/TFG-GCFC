fcbae9a60f89c2e7b095bcaeaabc1efdd6980346
RaulFSF <raulsf10@gmail.com>
2023-03-01 17:42:15 +0000

    Actualizar información de jugador

--- app/Filament/Resources/OwnPlayersResource/Pages/EditOwnPlayers.php
+++ app/Filament/Resources/OwnPlayersResource/Pages/EditOwnPlayers.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\OwnPlayersResource\Pages;
 
 use App\Filament\Resources\OwnPlayersResource;
 use App\Models\Player;
+use App\Models\User;
 use Filament\Notifications\Notification;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\EditRecord;
@@ -15,7 +16,32 @@ class EditOwnPlayers extends EditRecord
 
     protected function getActions(): array
     {
-        return [
-        ];
+        return [];
+    }
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        if ($this->record->user) {
+            if ($this->record->user->email != $data['email']) {
+                if (User::where('email', $data['email'])->first() == null) {
+                    $user = $this->record->user;
+                    $user->email = $data['email'];
+                    $user->name = $data['name'];
+                    $user->save();
+                } else {
+                    Notification::make()
+                        ->warning()
+                        ->title('Error al guardar los datos')
+                        ->persistent()
+                        ->send();
+                    $this->halt();
+                }
+            } elseif ($this->record->user->name != $data['name']) {
+                $user = $this->record->user;
+                $user->name = $data['name'];
+                $user->save();
+            }
+        }
+        return $data;
     }
 }
